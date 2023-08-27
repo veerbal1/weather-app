9e3b138b219f86194e5df7ef7c1fab235c1343bf
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-08-27 09:53:48 +0530

    Minor file renaming fix

---
+++ src/app.tsx
@@ -0,0 +1,23 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <h1
+        style={{
+          fontWeight: 200,
+          fontSize: "5em",
+        }}
+      >
+        SinghSky
+      </h1>
+    </div>
+  );
+}
+
+export default App;
