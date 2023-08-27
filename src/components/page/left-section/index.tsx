36dd52bce9b55b759d1fdc923b581afafb0acd20
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-08-27 10:38:15 +0530

    Added hardcoded temp and location

--- src/components/page/left-section/index.tsx
+++ src/components/page/left-section/index.tsx
@@ -1,7 +1,22 @@
 import style from "./style.module.css";
 
 const LeftSection = () => {
-  return <h1 className={style.section}></h1>;
+  return (
+    <div className={style.section}>
+      <h4 className={style.brandName}>SinghSky</h4>
+      <div className={style.temperatureSection}>
+        <h1 className={style.temperature}>16°</h1>
+        <div className={style.locationSTime}>
+          <h2>London</h2>
+          <h4>06:09 - Monday, 09 Sep '19</h4>
+        </div>
+        <div className={style.weatherIcon}>
+          <img src="" alt="" />
+          <h4>Cloudy</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftSection;
