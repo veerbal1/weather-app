8bc8f0c9610c0ced02ac1c0cb7b367a53256dda1
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-09-13 10:17:19 +0530

    Added temp weather video

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,7 @@ import RightSection from "./components/page/right-section";
 function App() {
   return (
     <div className="main">
+      <video src="./cloud-movie.mp4" autoPlay loop controls></video>
       <LeftSection />
       <RightSection />
     </div>
--- src/components/page/left-section/index.tsx
+++ src/components/page/left-section/index.tsx
@@ -5,10 +5,10 @@ const LeftSection = () => {
     <div className={style.section}>
       <h4 className={style.brandName}>SinghSky</h4>
       <div className={style.temperatureSection}>
-        <h1 className={style.temperature}>16°</h1>
+        <h1 className={style.temperature}>28°</h1>
         <div className={style.locationSTime}>
-          <h2>London</h2>
-          <h4>06:09 - Monday, 09 Sep '19</h4>
+          <h2>Chandigarh</h2>
+          <h4>11:14 - Sunday, 27 Aug '23</h4>
         </div>
         <div className={style.weatherIcon}>
           <img src="/cloud-icon.png" alt="Cloud Icon" />
