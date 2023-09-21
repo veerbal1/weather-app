12b969f9a4827113d1e13091458baa0e24c7d451
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-09-21 12:54:52 +0530

    Updated font styling

--- src/components/page/left-section/index.tsx
+++ src/components/page/left-section/index.tsx
@@ -6,11 +6,11 @@ const LeftSection = () => {
       <h4 className="text-4xl text-white">SinghSky</h4>
       <div className="flex items-end text-white gap-2">
         <h1 className="text-9xl text-white ">28°</h1>
-        <div>
-          <h2>Chandigarh</h2>
+        <div className="mb-2">
+          <h2 className="text-2xl">Chandigarh</h2>
           <h4>11:14 - Sunday, 27 Aug '23</h4>
         </div>
-        <div>
+        <div className="mb-2">
           <img src="/cloud-icon.png" alt="Cloud Icon" />
           <h4>Cloudy</h4>
         </div>
