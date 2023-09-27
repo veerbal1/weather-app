b674085373e080b684a97c056a422d01d73d3d2b
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-09-27 14:51:17 +0530

    Showind dummy search result

--- src/components/page/right-section/components/search/index.tsx
+++ src/components/page/right-section/components/search/index.tsx
@@ -1,8 +1,21 @@
 const Search = () => {
   return (
-    <div className="w-full h-16 pl-16 flex items-end justify-between">
-      <Input />
-      <Button />
+    <div className="w-full relative">
+      <div className="w-full h-16 pl-14 flex items-end justify-between">
+        <Input />
+        <Button />
+      </div>
+      <div className="w-full flex justify-center items-center mt-10">
+        <div className="w-full pl-14 flex flex-col justify-start">
+          {['Chandigarh', 'Mohali',"New York", "Ottawa"].map((item) => {
+            return (
+              <button key={item} className=" block text-left text-white text-lg font-thin my-2 hover:bg-black/20 transition">
+                {item}
+              </button>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
@@ -16,7 +29,13 @@ const Button = () => {
 };
 
 const Input = () => {
-  return <input type="text" className="w-3/4 bg-transparent border-b outline-none border-white text-white text-lg font-thin" placeholder="Location"/>;
+  return (
+    <input
+      type="text"
+      className="w-3/4 bg-transparent border-b outline-none border-white text-white text-lg font-thin"
+      placeholder="Location"
+    />
+  );
 };
 
 export default Search;
